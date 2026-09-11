533d3256dff0be3b37ed4215315cd9745fa455d4
raworiginal <118763950+raworiginal@users.noreply.github.com>
2026-09-11 14:47:39 -0700

    added better-auth

--- server/db/schema/index.ts
+++ server/db/schema/index.ts
@@ -1,15 +1,1 @@
-import {
-  integer,
-  pgTable,
-  varchar,
-  text,
-  timestamp,
-} from "drizzle-orm/pg-core";
-
-export const notesTable = pgTable("notes", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  title: varchar({ length: 255 }).notNull(),
-  body: text(),
-  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-  updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-});
+export * from "./auth-schema.ts";
--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,3 +1,6 @@
+import { PGlite } from "@electric-sql/pglite";
 import { drizzle } from "drizzle-orm/pglite";
+import * as schema from "../db/schema";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const client = new PGlite(process.env.DATABASE_URL!);
+export const db = drizzle(client, { schema });
