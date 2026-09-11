85b371b55919647a69bf2595a0dbd78d01eb1d46
raworiginal <118763950+raworiginal@users.noreply.github.com>
2026-09-11 15:50:01 -0700

    added server and client auth setup

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db.ts";
+import { username, admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  plugins: [username(), admin()],
 });
