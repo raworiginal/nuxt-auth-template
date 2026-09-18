7fc8dca9dfee2830c2415248dd4208a84cfac6e8
raworiginal <randall.a.wolfe@gmail.com>
2026-09-17 20:28:43 -0700

    login form works, now working on validation with zod

--- app/components/auth/LoginForm.vue
+++ app/components/auth/LoginForm.vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
+import { loginSchema } from "#shared/utils/authSchemas";
+
+const emailLogin = useSignIn("email");
+const usernameLogin = useSignIn("username");
+
 const form = reactive({
   identifier: "",
   password: "",
 });
 
+const isEmail = computed(() => form.identifier.trim().includes("@"));
+
+const error = computed(() =>
+  isEmail.value ? emailLogin.error.value : usernameLogin.error.value,
+);
+const isPending = computed(() =>
+  isEmail.value
+    ? emailLogin.status.value === "pending"
+    : usernameLogin.status.value === "pending",
+);
+
 async function login() {
-  if (form.identifier.includes("@")) {
-    console.log(`${form.identifier} is an email`);
-  } else {
-    console.log(`${form.identifier} is a username`);
+  const result = loginSchema.safeParse(form);
+
+  if (isEmail.value) {
+    console.log("email login");
+    await emailLogin.execute({
+      email: form.identifier,
+      password: form.password,
+    });
+    return;
   }
+  console.log("username login");
+  await usernameLogin.execute({
+    username: form.identifier,
+    password: form.password,
+  });
 }
+
+const logout = useSignOut().execute();
 </script>
 
 <template>
@@ -19,7 +47,9 @@ async function login() {
       class="fieldset bg-base-200 border-base-300 rounded-box w-sm border p-4"
     >
       <legend class="fieldset-legend">Login</legend>
-
+      <div v-if="error" role="alert" class="alert alert-error alert-soft">
+        <span>{{ error.message }}</span>
+      </div>
       <label class="label">Email or Username</label>
       <input
         v-model="form.identifier"
@@ -36,7 +66,12 @@ async function login() {
         placeholder="Password"
       />
 
-      <button class="btn btn-neutral mt-4">Login</button>
+      <button class="btn btn-neutral mt-4">
+        <span v-if="isPending" class="loading"></span>
+        <span v-else>Login</span>
+      </button>
     </fieldset>
   </form>
+
+  <button @click="logout" class="btn btn-error">logout</button>
 </template>
---
+++ shared/utils/authSchemas.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  identifier: z.string().trim().min(1, "Email or username is required"),
+  password: z.string().min(1, "Password is required"),
+});
